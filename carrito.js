1c67b86fe46515fc9261f61e8fb91b919e0ceb06
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-02-07 17:38:52 +0100

    Added new method for adding the property "quantity" to the list of products.

--- carrito.js
+++ carrito.js
@@ -1,8 +1,16 @@
 
 export class Carrito {
-    constructor(productos){
-        this.productos = "";
+     constructor(productos){
+        this.productos = productos["products"];
         this.total = 0;
-        this.currency = "";
+        this.currency = productos["currency"];
+        this.#initProductos()
+    }
+
+    #initProductos(){
+        // Añade el parametro quantity a cada producto
+        for(let producto of this.productos) {
+            producto["quantity"] = 1;
+        }
     }
 }
